4863342290c25f2017a71b0247a936fafbcf5d37
alek <user@user>
2022-12-11 15:21:48 +0300

    Write ApplicationContext and implementation of IGenericRepository.

---
+++ TrainingScheduler.DAL/Contexts/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingScheduler.DAL.Common.Models;
+
+namespace TrainingScheduler.DAL.Contexts
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<TrainingArchive> TrainingArchives { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
