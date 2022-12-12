035b5474ddcb00b1670d33e44abf86953f98d17c
alek <user@user>
2022-12-12 21:39:19 +0300

    Wrote ExerciseService. Nothing to test.

---
+++ TrainingScheduler.BLL/Services/ExerciseService.cs
@@ -0,0 +1,36 @@
+﻿using TrainingScheduler.BLL.Interfaces;
+using TrainingScheduler.DAL.Common.Interfaces.Repositories;
+using TrainingScheduler.DAL.Common.Models;
+
+namespace TrainingScheduler.BLL.Services
+{
+    public class ExerciseService : IExerciseService
+    {
+        private readonly IGenericRepository<Exercise> _genericRepository;
+
+        public ExerciseService(IGenericRepository<Exercise> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public void Add(Exercise exercise)
+        {
+            _genericRepository.Add(exercise);
+        }
+
+        public void Delete(Exercise exercise)
+        {
+            _genericRepository.Delete(exercise);
+        }
+
+        public IEnumerable<Exercise> GetAll()
+        {
+            return _genericRepository.GetAll();
+        }
+
+        public void Update(Exercise exercise)
+        {
+            _genericRepository.Update(exercise);
+        }
+    }
+}
